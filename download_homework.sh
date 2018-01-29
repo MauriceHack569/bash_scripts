d4b5c02eb94498498a2334b77919346ef0997b80
Maurice Washington <mauricehack569@gmail.com>
2018-01-28 22:19:52 -0500

    Modified file for readability

--- download_homework.sh
+++ download_homework.sh
@@ -1,98 +1,74 @@
 #!/bin/bash
 
-HOMEWORK_NUMBER=$@
+output_file=~/Desktop/bash_scripting/download_homework.out
 
-homeworkPDF="c4cs-wk$HOMEWORK_NUM-homework.pdf"
-homeworkTEX="c4cs-wk$HOMEWORK_NUM-homework.tex"
-advancedPDF="c4cs-wk$HOMEWORK_NUM-advanced.pdf"
-advancedTEX="c4cs-wk$HOMEWORK_NUM-advanced.tex"
+function exists {
+	echo "$(tput setaf 1)The file \"$1\" already exists."
+}
+function removing {
+	echo "Removing \"$1\" from current directory. $(tput setaf 2)Proceeding .....";
+}
+function doesnotexist {
+	echo "$(tput setaf 1)The file \"$1\" does not exist. $(tput setaf 2)Proceeding .....";
+}
+function moving {
+	echo "$(tput setaf 2)Moving \"$1\" to its destination .....";
+	if [ "$2" = "hw" ]; then
+		mv $1 ~/Desktop/eecs398/Homework/homework-$3;
+	elif [ "$2" = "adv" ]; then
+		mv $1 ~/Desktop/eecs398/Advanced/advanced-$3;
+	else 
+		echo "ERROR";
+	fi
+}
+function download {
+	echo "$(tput setaf 4)Downloading Homework $1 PDF Document..."
+	wget https://c4cs.github.io/static/w18/hw/c4cs-wk$1-homework.pdf 2>> $output_file
 
-for HOMEWORK_NUM in $HOMEWORK_NUMBER; do
-	tput setaf 3; echo "Downloading Homework $HOMEWORK_NUM PDF Document ....."
-	wget https://c4cs.github.io/static/w18/hw/c4cs-wk$HOMEWORK_NUM-homework.pdf 2>> ~/Desktop/bash_scripting/download_homework.out
+	echo "Downloading Homework $1 Tex Document..."
+	wget https://c4cs.github.io/static/w18/hw/c4cs-wk$1-homework.tex 2>> $output_file
 
-	tput setaf 3; echo "Downloading Homework $HOMEWORK_NUM Tex Document ....."
-	wget https://c4cs.github.io/static/w18/hw/c4cs-wk$HOMEWORK_NUM-homework.tex 2>> ~/Desktop/bash_scripting/download_homework.out
+	echo "Downloading Advanced Homework $1 PDF Document..."
+	wget https://c4cs.github.io/static/w18/advanced/c4cs-wk$1-advanced.pdf 2>> $output_file
 
-	tput setaf 3; echo "Downloading Advanced Homework $HOMEWORK_NUM PDF Document ....."
-	wget https://c4cs.github.io/static/w18/advanced/c4cs-wk$HOMEWORK_NUM-advanced.pdf 2>> ~/Desktop/bash_scripting/download_homework.out
-
-	tput setaf 3; echo "Downloading Advanced Homework $HOMEWORK_NUM Tex Document ....."
-	wget https://c4cs.github.io/static/w18/advanced/c4cs-wk$HOMEWORK_NUM-advanced.tex 2>> ~/Desktop/bash_scripting/download_homework.out
-
-	for type in $homeworkPDF $homeworkTEX $advancedPDF $advancedTEX; do
+	echo "Downloading Advanced Homework $1 Tex Document..."
+	wget https://c4cs.github.io/static/w18/advanced/c4cs-wk$1-advanced.tex 2>> $output_file	
+}
+function check {
+	for type in "$homeworkPDF" "$homeworkTEX" "$advancedPDF" "$advancedTEX"; do
 		if [[ "$type" = "$homeworkPDF" || "$type" = "$homeworkTEX" ]]; then
 			if [ -e ~/Desktop/eecs398/Homework/homework-$HOMEWORK_NUM/$type ]; then
-				tput setaf 1; echo "The file \"$type\" already exists. Removing file from current directory.";
-				rm $type;
-				tput setaf 2; echo "Proceeding .....";
+				exists $type;
+				removing $type;
+				rm $type; 
 			elif [ -e $type ]; then
-				mv $type ~/Desktop/eecs398/Homework/homework-$HOMEWORK_NUM;
+				moving $type "hw" $HOMEWORK_NUM;
 			else
-				tput setaf 1; echo "The file \"$type\" does not exist. Proceeding .....";
+				doesnotexist $type;
 			fi
 		elif [[ "$type" = "$advancedPDF" || "$type" = "$advancedTEX" ]]; then
 			if [ -e ~/Desktop/eecs398/Advanced/advanced-$HOMEWORK_NUM/$type ]; then
-				tput setaf 1; echo "The file \"$type\" already exists. Removing file from current directory.";
-				rm $type;
-				tput setaf 2; echo "Proceeding .....";
+				exists $type;
+				removing $type;
+				rm $type; 
 			elif [ -e $type ]; then
-				mv $type ~/Desktop/eecs398/Advanced/advanced-$HOMEWORK_NUM;
+				moving $type "adv" $HOMEWORK_NUM;
 			else
-				tput setaf 1; echo "The file \"$type\" does not exist. Proceeding .....";
+				doesnotexist $type;
 			fi
 		else
-			echo "What happened!";
+			exit 1;
 		fi
 	done
+}
 
+for HOMEWORK_NUM in $@; do
+	homeworkPDF="c4cs-wk$HOMEWORK_NUM-homework.pdf"
+	homeworkTEX="c4cs-wk$HOMEWORK_NUM-homework.tex"
+	advancedPDF="c4cs-wk$HOMEWORK_NUM-advanced.pdf"
+	advancedTEX="c4cs-wk$HOMEWORK_NUM-advanced.tex"
 
-	# ##########################################################################################
-
-	# if [ -e ~/Desktop/eecs398/Homework/homework-$HOMEWORK_NUM/$type ]; then
-	# 	tput setaf 1; echo "The file \"$type\" already exists. Removing file from current directory.";
-	# 	rm $type;
-	# 	tput setaf 2; echo "Proceeding .....";
-	# elif [ -e $type ]; then
-	# 	mv $type ~/Desktop/eecs398/Homework/homework-$HOMEWORK_NUM;
-	# else
-	# 	tput setaf 1; echo "The file \"$type\" does not exist. Proceeding .....";
-	# fi
-
-	# ##########################################################################################
-
-	# if [ -e ~/Desktop/eecs398/Homework/homework-$HOMEWORK_NUM/$homeworkTEX ]; then
-	# 	tput setaf 1; echo "The file \"$homeworkTEX\" already exists. Removing file from current directory.";
-	# 	rm $homeworkTEX;
-	# 	tput setaf 2; echo "Proceeding .....";
-	# elif [ -e $homeworkTEX ]; then
-	# 	mv $homeworkTEX ~/Desktop/eecs398/Homework/homework-$HOMEWORK_NUM;
-	# else
-	# 	tput setaf 1; echo "The file \"$homeworkTEX\" does not exist. Proceeding .....";
-	# fi
-
-	# ##########################################################################################
-
-	# if [ -e ~/Desktop/eecs398/Advanced/advanced-$HOMEWORK_NUM/$advancedPDF ]; then
-	# 	tput setaf 1; echo "The file \"$advancedPDF\" already exists. Removing file from current directory.";
-	# 	rm $advancedPDF;
-	# 	tput setaf 2; echo "Proceeding .....";
-	# elif [ -e $advancedPDF ]; then
-	# 	mv $advancedPDF ~/Desktop/eecs398/Advanced/advanced-$HOMEWORK_NUM;
-	# else
-	# 	tput setaf 1; echo "The file \"$advancedPDF\" does not exist. Proceeding .....";
-	# fi
-
-	# ##########################################################################################
-
-	# if [ -e ~/Desktop/eecs398/Advanced/advanced-$HOMEWORK_NUM/$advancedTEX ]; then
-	# 	tput setaf 1; echo "The file \"$advancedTEX\" already exists. Removing file from current directory.";
-	# 	rm $advancedTEX;
-	# 	tput setaf 2; echo "Proceeding .....";
-	# elif [ -e $advancedTEX ]; then
-	# 	mv $advancedTEX ~/Desktop/eecs398/Advanced/advanced-$HOMEWORK_NUM;
-	# else
-	# 	tput setaf 1; echo "The file \"$advancedTEX\" does not exist. Proceeding .....";
-	# fi
+	download $HOMEWORK_NUM;
+	check
 done
 
